7fc8f6a52899418c1e5f3ab4f4f8e470e67662e7
Jia De <cheokjiade@gmail.com>
2013-03-09 01:29:48 +0800

    integration of junes codes

---
+++ SMSserver/src/interfaces/SMS.java
@@ -0,0 +1,38 @@
+package interfaces;
+
+public class SMS {
+	String messageContents;
+	String phoneNumber;
+	static int counter=0;
+	int id;
+	//getters and setters here
+	public SMS(String messageContents, String phoneNumber) {
+		this.messageContents = messageContents;
+		this.phoneNumber = phoneNumber;
+		counter++;
+		id = counter;
+	}
+	
+	public String getMessageContents() {
+		return messageContents;
+	}
+	public void setMessageContents(String messageContents) {
+		this.messageContents = messageContents;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	
+}
---
+++ SMSserver/src/interfaces/SMSMessage.java
@@ -0,0 +1,75 @@
+package interfaces;
+
+import java.util.ArrayList;
+
+public class SMSMessage {
+	
+	public ArrayList<SMS> convertToSMS(String id, String type, String location, String time, String status, String phoneNo){
+		
+		String messageContents;
+		String phoneNumber;
+		int lastIndexOfSMS = 0;
+		int maxSMSlen = 140;
+		ArrayList<SMS> smsArrayList = new ArrayList<SMS>();
+		
+		messageContents = type + " @ " + location + "\nStatus: " + status + "\nReported by " + phoneNo + " @ " + time + "\nRef No.: " + id;
+		phoneNumber = phoneNo;
+		
+		lastIndexOfSMS = messageContents.length();
+		int startPoint = 0; //start of string to print
+		for(int i = 1; i <= lastIndexOfSMS / 140 + 1; i++){ //first string = 0 -> 140, second string = 141 -> 280, 281 -> end
+			if(i - lastIndexOfSMS / 140 +1 <= 1){ //first 140 string
+				maxSMSlen= i * 140;
+				System.out.println("\n" + messageContents.substring(startPoint, maxSMSlen)); 
+				smsArrayList.add(new SMS(messageContents.substring(startPoint, maxSMSlen), phoneNumber));
+				startPoint += 140;
+			}
+			
+			else{ //print last set of records
+				System.out.println("\n" + messageContents.substring(startPoint, lastIndexOfSMS)); 
+				smsArrayList.add(new SMS(messageContents.substring(startPoint, lastIndexOfSMS), phoneNumber));
+			}
+		}
+		
+		//logic to convert input to a 140 character smsobject
+		return smsArrayList;
+	}
+	
+	
+public ArrayList<SMS> convertToSMS(String id, String type, String location, String time, String status, String phoneNo, String weather, String windspeed){
+		
+		String messageContents;
+		String phoneNumber;
+		int lastIndexOfSMS = 0;
+		int maxSMSlen = 140;
+		ArrayList<SMS> smsArrayList = new ArrayList<SMS>();
+		messageContents = type + " @ " + location + "\nStatus: " + status + "\nReported by " + phoneNo + " @ " + time + "\nWeather: "+ weather + "  WindSpd: " + windspeed + "\nRef No.: " + id;
+		phoneNumber = phoneNo;
+		
+		lastIndexOfSMS = messageContents.length();
+		int startPoint = 0; //start of string to print
+		for(int i = 1; i <= lastIndexOfSMS / 140 + 1; i++){ //first string = 0 -> 140, second string = 141 -> 280, 281 -> end
+			if(i - lastIndexOfSMS / 140 + 1 <= 1){ //first 140 string
+				maxSMSlen= i * 140;
+				System.out.println("\n" + messageContents.substring(startPoint, maxSMSlen)); 
+				smsArrayList.add(new SMS(messageContents.substring(startPoint, maxSMSlen), phoneNumber));
+				startPoint += 140;
+			}
+			
+			else{ //print last set of records
+				System.out.println("\n" + messageContents.substring(startPoint, lastIndexOfSMS)); 
+				smsArrayList.add(new SMS(messageContents.substring(startPoint, lastIndexOfSMS), phoneNumber));
+			}
+		}
+		
+		//logic to convert input to a 140 character smsobject
+		return smsArrayList;
+	}
+	
+	
+	public static void main(String[] args){
+		SMSMessage message = new SMSMessage();
+		message.convertToSMS("1", "FIRE", "AMK AMK AMK AMK AMK AMK AMK  ", "12 noon", "info info info info info ","123123123", "Sunny", "123KM/h");
+		
+	}
+}
