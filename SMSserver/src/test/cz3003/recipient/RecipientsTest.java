9568c3dea1913ffea9b484ed4682e02e50f693fe
Jia De <cheokjiade@gmail.com>
2013-03-24 01:56:20 +0800

    added more unit tests

---
+++ SMSserver/src/test/cz3003/recipient/RecipientsTest.java
@@ -0,0 +1,21 @@
+package test.cz3003.recipient;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class RecipientsTest {
+
+	@Test
+	public void testRecipients() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSelectNumberBasedOnIncidentType() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ SMSserver/src/test/cz3003/server/LoadBalancerTest.java
@@ -0,0 +1,54 @@
+package test.cz3003.server;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class LoadBalancerTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testLoadBalancer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSendMessageOut() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testChooseBestClient() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSendMessageToClient() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCreateMessageToSMSIntStringStringStringString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCreateMessageToSMSCPUMessage() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateRecipientList() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSendErrorReport() {
+		fail("Not yet implemented");
+	}
+
+}
